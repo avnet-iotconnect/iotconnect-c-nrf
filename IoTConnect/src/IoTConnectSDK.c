c92db9070af89c888343550e98e48041cda20cba
sw-iot-himanshuparmar1 <166377257+sw-iot-himanshuparmar1@users.noreply.github.com>
2024-12-17 12:49:10 +0530

    Update IoTConnectSDK.c
    Signed-off-by: sw-iot-himanshuparmar1 <166377257+sw-iot-himanshuparmar1@users.noreply.github.com>

--- IoTConnect/src/IoTConnectSDK.c
+++ IoTConnect/src/IoTConnectSDK.c
@@ -830,7 +830,7 @@ char* get_base_url(char*Host, char *cpid, char *env)
 
     if (Base_URL != NULL)
     {
-        strcat(Base_URL,"sync");
+        // strcat(Base_URL,"sync");
         return Base_URL;
     }
     else
